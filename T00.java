f05f109cc8569951e26b8de47b39d655b68a3a5b
Gabz1boi <gabrielignasiussinaga000@gmail.com>
2025-10-03 11:35:18 +0700

    ULANG 1

--- T00.java
+++ T00.java
@@ -3,38 +3,38 @@
 import java.util.*;
 import java.lang.Math;
 
-public class T00 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         int x, y;
 
-        x = input.nextInt();
-        y = input.nextInt();
+        x = Integer.parseInt(input.nextLine());
+        y = Integer.parseInt(input.nextLine());
         if (x % 2 == 0) {
-            System.out.println(" Bilangan pertama: Genap ");
+            System.out.println("Bilangan pertama: Genap");
         } else {
-            System.out.println(" Bilangan pertama: Ganjil ");
+            System.out.println("Bilangan pertama: Ganjil");
         }
         if (y % 2 == 0) {
-            System.out.println(" Bilangan kedua: Genap ");
+            System.out.println("Bilangan kedua: Genap");
         } else {
-            System.out.println(" Bilangan kedua: Ganjil");
+            System.out.println("Bilangan kedua: Ganjil");
         }
         if (x > y) {
-            System.out.println(" Bilangan pertama lebih besar ");
+            System.out.println("Bilangan pertama lebih besar");
         }
         if (x < y) {
-            System.out.println(" Bilangan kedua lebih besar ");
+            System.out.println("Bilangan kedua lebih besar");
         }
         if (y == x) {
-            System.out.println(" Kedua Bilangan sama besar ");
+            System.out.println("Kedua Bilangan sama besar");
         }
         if (x % 2 == 0 && y % 2 == 0) {
-            System.out.println(" Hasil Penjumlahan :" + x + y);
+            System.out.println("Hasil Penjumlahan:" + x + y);
         }
         if (x % 2 != 0 && y % 2 != 0) {
-            System.out.println("Hasil Perkalian: " + x * y);
+            System.out.println("Hasil Perkalian:" + x * y);
         }
         if (x % 2 != 0 && y % 2 == 0) {
             System.out.println("Berbeda Jenis");
