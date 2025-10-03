ebbaa26257df587c94499b8fb92c83b904d806bd
Gabz1boi <gabrielignasiussinaga000@gmail.com>
2025-10-03 09:22:37 +0700

    TES 10

--- T00.java
+++ T00.java
@@ -1,14 +1,16 @@
+// 12S25021-Gabriel Ignasius Sinaga 12S25030-Nina Theresia Tarigan
+
 import java.util.*;
 import java.lang.Math;
 
-public class Program {
+public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         int x, y;
 
-        x = input.nextInt();
-        y = input.nextInt();
+        x = Integer.parseInt(input.nextLine());
+        y = Integer.parseInt(input.nextLine());
         if (x % 2 == 0) {
             System.out.println(" Bilangan pertama: Genap ");
         } else {
@@ -21,21 +23,24 @@ public class Program {
         }
         if (x > y) {
             System.out.println(" Bilangan pertama lebih besar ");
-        } else {
-            if (y > x) {
-                System.out.println(" Bilangan kedua lebih besar ");
-            } else {
-                System.out.println(" Kedua Bilangan sama besar ");
-            }
+        }
+        if (x < y) {
+            System.out.println(" Bilangan kedua lebih besar ");
+        }
+        if (y == x) {
+            System.out.println(" Kedua Bilangan sama besar ");
         }
         if (x % 2 == 0 && y % 2 == 0) {
             System.out.println(" Hasil Penjumlahan :" + x + y);
-        } else {
-            if (x % 2 != 0 && y % 2 != 0) {
-                System.out.println("Hasil Perkalian: " + x * y);
-            } else {
-                System.out.println("Berbeda Jenis");
-            }
+        }
+        if (x % 2 != 0 && y % 2 != 0) {
+            System.out.println("Hasil Perkalian: " + x * y);
+        }
+        if (x % 2 != 0 && y % 2 == 0) {
+            System.out.println("Berbeda Jenis");
+        }
+        if (x % 2 == 0 && y % 2 != 0) {
+            System.out.println("Berbeda Jenis");
         }
     }
 }
